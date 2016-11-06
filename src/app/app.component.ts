1da418cfbda68d8d07cdb07b565ecc04aebd3ef6
NablaT <remi.pourtier@laposte.net>
2016-11-06 19:12:12 +0100

    Adding setting menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import {MdSidenav} from "@angular/material";
 
 @Component({
   selector: 'app-root',
@@ -7,21 +8,33 @@ import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 })
 export class AppComponent {
 
-  dogs = [
-    {rows: 2, fileName:"broken-back", name: "Mal", human: "Jeremy", age: 5},
-    {rows: 1, fileName:"coldplay", name: "Molly", human: "David", age: 5},
-    { rows: 1, fileName:"flying-lotus", name: "Sophie", human: "Alex", age: 8},
-    {rows: 2, fileName:"jar-of-flies", name: "Taz", human: "Joey", age: '11 weeks'},
-    { rows: 1, fileName:"joy-division", name: "Kobe", human: "Igor", age: 5},
-    {rows: 2, fileName:"julien-dore", name: "Porter", human: "Kara", age: 3},
-    { rows: 1, fileName:"nirvana", name: "Stephen", human: "Stephen", age: 8},
-    {rows: 1, fileName:"pink-floyd", name: "Cinny", human: "Jules", age: 3},
-    { rows: 1, fileName:"beast-night", name: "Hermes", human: "Kara", age: 3},
+  albums = [
+    {rows: 2, fileName:"broken-back", name: "Mal", artist: "Jeremy", date: 5},
+    {rows: 1, fileName:"coldplay", name: "Molly", artist: "David", date: 5},
+    { rows: 1, fileName:"flying-lotus", name: "Sophie", artist: "Alex", date: 8},
+    {rows: 2, fileName:"jar-of-flies", name: "Taz", artist: "Joey", date: '11 weeks'},
+    { rows: 1, fileName:"joy-division", name: "Kobe", artist: "Igor", date: 5},
+    {rows: 2, fileName:"julien-dore", name: "Porter", artist: "Kara", date: 3},
+    { rows: 1, fileName:"nirvana", name: "Stephen", artist: "Stephen", date: 8},
+    {rows: 1, fileName:"pink-floyd", name: "Cinny", artist: "Jules", date: 3},
+    { rows: 1, fileName:"beast-night", name: "Hermes", artist: "Kara", date: 3},
   ];
 
-  constructor() {}
+  @ViewChild('sidenav') sidenav: MdSidenav;
+  private selectedAlbum;
 
-  showInformation(album:string){
+  constructor() {
+    this.selectedAlbum={rows: 0,
+      fileName:"", name: "", artist: "", date: 0};
+  }
 
+  showInformation(album){
+    console.log("yes");
+    this.selectedAlbum=album;
+    this.sidenav.open();
+  }
+
+  hideInformation(){
+    this.sidenav.close();
   }
 }
